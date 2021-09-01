b55e9e3d5b8a2af83add610d83eb3e562f090f40
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-01 17:55:11 +0200

    Added all components, and styled the Nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import TechSection from "./components/TechSection";
+import ContactSection from "./components/ContactSection";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <ScrollToTop />
+        <NavBar />
+      </BrowserRouter>
+      <div className="content-container">
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <TechSection />
+        <ContactSection />
+      </div>
     </div>
   );
 }
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProjectsSection() {
+  return (
+    <div className='project-container' id="projects">
+      <h1>Projects</h1>
+    </div>
+  );
+}
