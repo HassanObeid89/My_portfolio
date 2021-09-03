d89fd35577099ec9a79cd460957fcfbe4fc52bbc
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-03 12:43:19 +0200

    Added the modal component, and styled it.

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -10,41 +10,58 @@ export const data= [
   {
     "id": "1",
     "title": "Shopping list",
-    "image": shoppingList
+    "image": shoppingList,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form'],
+    "completed":true
   },
   {
     "id": "2",
     "title": "Vegan restaurant website",
-    "image": vegan
+    "image": vegan,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "3",
     "title": "Package tracker",
-    "image": package_tracker
+    "image": package_tracker,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "4",
     "title": "Todo list TTD",
-    "image": todo_list
+    "image": todo_list,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "5",
     "title": "E-commerce site",
-    "image": Ecommerce
+    "image": Ecommerce,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "6",
     "title": "BBQ restaurant",
-    "image": BBQ
+    "image": BBQ,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "7",
     "title": "Learning platform",
-    "image": learning_platform
+    "image": learning_platform,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   },
   {
     "id": "5",
     "title": "Netflix clone",
-    "image": netflix
+    "image": netflix,
+    "description":"This project is about A, B, C where users can do D, E, F, so they can obtain G, H, I benefit. It uses:",
+    "pills":['React', 'FireBase', 'Tailwind CSS', 'Yup', 'React-hook-form']
   }
 ]
