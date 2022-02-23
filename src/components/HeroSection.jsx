a8c48a88e8e1e56294ab6d1d836a2ac38b8830a7
HassanObeid89 <hassan.obeid.ho@hotmail.com>
2022-02-23 11:05:04 +0100

    Changed hero image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,12 @@
+import hero from "../assets/hero_4.jpg";
 export default function HeroSection() {
   return (
     <div id="hero" className="hero-container">
-      <h1>Hasse Obeid, Aspiring Front-End developer</h1>
+      <img src={hero} />
+      <section className="slogen">
+        <h1>Hasse Obeid, </h1>
+        <p>Aspiring Front-End developer</p>
+      </section>
     </div>
   );
 }
